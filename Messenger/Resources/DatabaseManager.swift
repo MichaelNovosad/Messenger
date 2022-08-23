dd5c22b92e84ddfadf5b435db10051c8a2b8c3e8
MichaelNovosad <m.novosad@artk.ai>
2022-08-23 15:26:34 +0300

    Photo messages in chat

--- Messenger/Resources/DatabaseManager.swift
+++ Messenger/Resources/DatabaseManager.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import FirebaseDatabase
+import MessageKit
 
 final class DatabaseManager {
     
@@ -345,10 +346,29 @@ extension DatabaseManager {
                       let date = ChatViewController.dateFormatter.date(from: dateString) else {
                     return nil
                 }
+                
+                var kind: MessageKind?
+                if type == "photo" {
+                    guard let imageUrl = URL(string: content),
+                          let placeholder = UIImage(systemName: "plus")
+                    else { return nil}
+                    
+                    let media = Media(url: imageUrl,
+                                      image: nil,
+                                      placeholderImage: placeholder,
+                                      size: CGSize(width: 300, height: 300))
+                    kind = .photo(media)
+                }
+                else {
+                    kind = .text(content)
+                }
+                
+                guard let finalKind = kind else { return nil }
+                
                 let sender = Sender(photoURL: "",
                                     senderId: senderEmail,
                                     displayName: name)
-                return Message(sender: sender, messageId: messageId, sentDate: date, kind: .text(content))
+                return Message(sender: sender, messageId: messageId, sentDate: date, kind: finalKind)
                 
                 
             }
@@ -380,6 +400,10 @@ extension DatabaseManager {
             switch newMessage.kind {
             case .text(let messageText):
                 message = messageText
+            case .photo(let mediaItem):
+                if let targetUrlString = mediaItem.url?.absoluteString {
+                message = targetUrlString
+                }
             default:
                 break
             }
--- Messenger/Resources/StorageManager.swift
+++ Messenger/Resources/StorageManager.swift
@@ -40,6 +40,32 @@ final class StorageManager {
         }
     }
     
+    /// Upload image that will be sent in a conversation message
+    public func uploadMessagePhoto(with data: Data, fileName: String, completion: @escaping UploadPictureCompletion) {
+        
+        storage.child("message_images/\(fileName)").putData(data, metadata: nil) { [weak self] metadata, error in
+            guard let strongSelf = self else { return }
+            guard error == nil else {
+                print("Failed to upload picture to firebase storage")
+                completion(.failure(StorageErrors.failedToUpload))
+                return
+            }
+            
+            strongSelf.storage.child("message_images/\(fileName)").downloadURL { url, error in
+                guard let url = url else {
+                    print("Failed to get download URL")
+                    completion(.failure(StorageErrors.failedToGetDownloadURL))
+                    return
+                }
+                
+                let urlString = url.absoluteString
+                print("Download url returned:", urlString)
+                completion(.success(urlString))
+            }
+        }
+    }
+    
+    
     public enum StorageErrors: Error {
         case failedToUpload
         case failedToGetDownloadURL
